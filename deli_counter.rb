f4efe5e71cf4c1ab5e87d1478d6106a5b6cb4383
Morgan Collado <morgan.collado@gmail.com>
2020-05-12 02:20:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,8 +20,8 @@ def take_a_number(katz_deli, name)
 end 
 
 def now_serving(katz_deli)
-  puts "Currently serving #{katz_deli.shift}."
   if katz_deli.size == 0
-    puts 
-  
+    puts "There is nobody waiting to be served!"
+  else puts "Currently serving #{katz_deli.shift}."
+  end 
 end 
