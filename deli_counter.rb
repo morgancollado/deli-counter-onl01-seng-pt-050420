2ee3fdf5992a6c5772686450a847c6708a1dcac3
Morgan Collado <morgan.collado@gmail.com>
2020-05-12 01:34:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,9 @@ def line(katz_deli)
    new_array = ["The line is currently:"]
    line = 1 
    katz_deli.each do |customer|
-     new_array.push
+     new_array.push("#{line}. #{customer}")
+        line += 1
+      end 
   if katz_deli.size == 0
     puts "The line is currently empty."
   end 
