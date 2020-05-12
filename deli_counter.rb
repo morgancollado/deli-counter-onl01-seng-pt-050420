691aea289ef8fc73080f687078b752e1c223abe3
Morgan Collado <morgan.collado@gmail.com>
2020-05-12 01:51:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def line(katz_deli)
 end 
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(#{name})
+  katz_deli.push(name)
   puts "Welcome #{name}. You are #{index + 1} in line."
   
 end 
